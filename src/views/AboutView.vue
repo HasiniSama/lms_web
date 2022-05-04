9903076e4c2d4b86c4ff1f35d8801c5a676ded7b
nsadisha <nsadisha@gmail.com>
2022-05-04 15:22:10 +0530

    testing bootstrap

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -39,5 +39,23 @@
         </div>
       </div>
     </div>
+
+    <button type="button" class="btn btn-primary" id="liveToastBtn">Show live toast</button>
+
+    <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
+      <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="toast-header">
+          <img src="../assets/logo.png" class="rounded me-2" alt="...">
+          <strong class="me-auto">Bootstrap</strong>
+          <small>11 mins ago</small>
+          <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+        <div class="toast-body">
+          Hello, world! This is a toast message.
+        </div>
+      </div>
+    </div>
+
+
   </div>
 </template>
