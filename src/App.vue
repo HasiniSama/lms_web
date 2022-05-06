ed4ccf7fb76cffd5c852714d07288aa6848243a5
nsadisha <nsadisha@gmail.com>
2022-05-07 00:38:07 +0530

    changed fonts

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
 @import url('assets/css/styles.css');
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'sf-pro-light', Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
