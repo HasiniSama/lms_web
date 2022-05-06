6a138a4849a3a9769f8667dbcbf01a0778d846a6
Hasini <hasinisama99@gmail.com>
2022-05-06 13:09:20 +0530

    Added carousel

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
-  <nav>
+  <NavBarVue/>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </nav>
+  </nav> -->
   <router-view/>
 </template>
+
+<script>
+import NavBarVue from '@/components/NavBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBarVue
+  }
+}
+</script>
 
 <style>
 #app {
@@ -13,18 +25,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
