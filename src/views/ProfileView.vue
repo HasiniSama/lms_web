25c97974e0706a6a71cacffd4b46c50822cbeaf6
tchamalsha <tharu.chamalsha@gmail.com>
2022-05-06 15:40:07 +0530

    add avatar

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class="home">
-    
+  <div class="container profile">
+    <div class="row justify-content-center">
+          <div class="col-md-4 ml-5">
+            <img src="../assets/owl.png" alt="">
+          </div>
+          <h2>About me</h2>
+          <div class="col-md-7">
+            <p class="pt-3" data-aos="fade-down">I am a goal-oriented individual with the ability to cope up with any responsibility and duty given to me. 
+              I have the capability of interacting with various types of people and working together as a team
+               with having a good team spirit..</p>
+            </div>
+    </div>
   </div>
 </template>
 
