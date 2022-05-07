a041a6dc585b9556598dc77f2ffd047bc1ff8bf1
Hasini <hasinisama99@gmail.com>
2022-05-07 15:01:21 +0530

    Changed nav bar text

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,17 @@
 /* Font Awesome */
 @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css');
 
+@font-face {
+  font-family: "sf-pro";
+  src: local("sf-pro"),
+   url('assets/font/sf-pro.otf') format("truetype");
+}
+
 /* Apply global css classes */
 @import url('assets/css/styles.css');
 
 #app {
-  font-family: 'sf-pro-light', Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'sf-pro', Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
