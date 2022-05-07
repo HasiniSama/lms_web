99772f5997b28f4ae6d5219291f61b1aa3df5834
nsadisha <nsadisha@gmail.com>
2022-05-07 20:31:14 +0530

    course details page

---
+++ src/views/CourseDetailsView.vue
@@ -0,0 +1,40 @@
+<template>
+    <NavBarVue />
+    <section class="page-content">
+        <!-- <CourseDetailsViewForEveryone :id="id" /> -->
+        <CourseDetailsForEnrolledStudents :id="id" />
+        <!-- <CourseDetailsForConductingLecturers id="33" /> -->
+    </section>
+</template>
+
+<script>
+// import nav bar
+import NavBarVue from '@/components/NavBar.vue'
+
+// import course views
+import CourseDetailsViewForEveryone from './course/CourseDetailsViewForEveryone.vue'
+import CourseDetailsForEnrolledStudents from './course/CourseDetailsViewForEnrolledStudents.vue'
+import CourseDetailsForConductingLecturers from './course/CourseDetailsForConductingLecturers.vue'
+
+export default {
+    name: 'CourseDetails',
+    components: {
+        NavBarVue,
+        CourseDetailsViewForEveryone,
+        CourseDetailsForEnrolledStudents,
+        CourseDetailsForConductingLecturers
+    },
+    data(){
+        return{
+            id: this.$route.params.id
+        }
+    },
+    created(){
+        // this.id = this.$route.params.id
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
