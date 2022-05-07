638b89c26881886b41912d6acff21b8bbfb62c38
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2022-05-07 22:39:08 +0530

    Update the course page

--- src/views/CoursesView.vue
+++ src/views/CoursesView.vue
@@ -1,5 +1,44 @@
 <template>
-    <section>
-
+    <section class="courses">
+        <div class="container-fluid">
+            <div class="row">
+            
+            </div>
+        </div>
     </section>
 </template>
+
+<script>
+import NavBarVue from '@/components/NavBar.vue'
+
+export default {
+  name: 'Courses',
+  components: {
+    NavBarVue
+  }
+}
+</script>
+
+<style scoped>
+    img{
+        width: 70%;
+        height: auto;
+    }
+    .btn-brown{
+        background-color: #946B2D;
+        color: white;
+    }
+    .bottom-text{
+        font-size: 0.9rem;
+    }
+    label{
+        margin-bottom: 0;
+    }
+    span{
+        font-weight: bold;
+    }
+    i{
+        font-size: 85%;
+        margin: auto 5px;
+    }
+</style>
