a659b2f09bb19b86243676fe8c38921ed578f6f9
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2022-05-08 18:02:33 +0530

    Added 2 rows of course view page

--- src/views/CoursesView.vue
+++ src/views/CoursesView.vue
@@ -2,8 +2,22 @@
     <!-- <NavBarVue/> -->
     <section class="courses">
         <div class="container-fluid">
+            <h1>Enrolled Courses</h1>
             <div class="row">
-                <h1>Enrolled Courses</h1>
+                <div class="col-sm-12 col-md-3 mx-auto align-items-center">
+                    <CardVue/>
+                </div>
+                <div class="col-sm-12 col-md-3 mx-auto align-items-center">
+                    <CardVue/>
+                </div>
+                <div class="col-sm-12 col-md-3 mx-auto align-items-center">
+                    <CardVue/>
+                </div>
+                <div class="col-sm-12 col-md-3 mx-auto align-items-center">
+                    <CardVue/>
+                </div>
+            </div>
+            <div class="row">
                 <div class="col-sm-12 col-md-3 mx-auto align-items-center">
                     <CardVue/>
                 </div>
@@ -35,6 +49,10 @@ export default {
 </script>
 
 <style scoped>
+    .courses {
+        background-color: #dddddd;
+        padding: 30px 0;
+    }
     .courses h1 {
         font-size: 28px;
         text-align: center;
