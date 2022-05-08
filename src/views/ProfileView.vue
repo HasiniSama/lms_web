2526432c17d584cfe34b1722153696be2ec4a8b2
tchamalsha <tharu.chamalsha@gmail.com>
2022-05-08 08:50:46 +0530

    add tabs animation

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -2,7 +2,7 @@
   <div class="container profile p-5">
     <div class="row justify-content-center">
           <div class="col-md-4 ">
-            <img src="../assets/owl.png" alt="">
+            <img src="../assets/profile/owl.png" alt="">
           </div>
           <div class="sub-head p-2">
             <h3  id="name">Tharushi Chamalsha</h3>
@@ -10,27 +10,25 @@
           </div>
     </div>
     <div class="row justify-content-center">
-        <div class="col-md-4">
+        <div class="col-sm-6">
             <ul class="nav nav-pills mb-3 flex-column flex-sm-row" id="pills-tab" role="tablist">
                 <li class="nav-item" role="presentation">
-                    <button class="flex-sm-fill nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true"><span>About me</span></button>
+                    <button class="flex-sm-fill nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-about" type="button" role="tab" aria-controls="pills-home" aria-selected="true"><span>About me</span></button>
                 </li>
                 <li class="nav-item" role="presentation">
                     <button  class="flex-sm-fill nav-link  " id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false"><span>Grades</span></button>
                 </li>
                 <li class="nav-item" role="presentation">
-                    <button class="flex-sm-fill nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false"><span>About me</span></button>
+                    <button class="flex-sm-fill nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-edit" type="button" role="tab" aria-controls="pills-contact" aria-selected="false"><span>Edit Profile</span></button>
                 </li>
-                <li class="nav-item" role="presentation">
-                    <button class="flex-sm-fill nav-link" id="pills-more-tab" data-bs-toggle="pill" data-bs-target="#pills-more" type="button" role="tab" aria-controls="pills-contact" aria-selected="false"><span>More</span></button>
-                </li>
+                
             </ul>
         </div>
     </div>
     <div class="row p-3 justify-content-center">
         <div class="col-md-8 text-start">
             <div class="tab-content" id="pills-tabContent">
-                <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+                <div class="tab-pane fade show active about" id="pills-about" role="tabpanel" aria-labelledby="pills-home-tab">
                     <form>
                         <fieldset disabled>
                            
@@ -68,7 +66,7 @@
                     </div>
                     <div id="curve_chart"></div>
                 </div>
-                <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
+                <div class="tab-pane fade" id="pills-edit" role="tabpanel" aria-labelledby="pills-contact-tab">
                      <form>
                         <fieldset>
                            
@@ -122,16 +120,38 @@
 .profile .sub-head h3{
     color: black;
 }
-
-.nav .nav-item button.active{
+.nav .nav-item button{
     background-color: transparent;
-    color: #946B2D;
+    color: #5D5D5D;
    
 }
-
-.nav .nav-item button.active span{
-    padding: 2px;
-    border-bottom: 3px black solid;
+.nav .nav-item button.active{
+    font-weight:bold;
+    color: #946B2D;
+    
 }
 
+.nav li {
+  display: inline-block;
+  position: relative;
+  padding-bottom: 3px;
+  margin-right: 10px;
+}
+.nav li:last-child {
+  margin-right: 0;
+}
+
+.nav li:after {
+  content: '';
+  display: block;
+  margin: auto;
+  height: 3px;
+  width: 0px;
+  background: transparent;
+  transition: width .5s ease, background-color .5s ease;
+}
+.nav li:hover:after {
+  width: 100%;
+  background: #222F5B;
+}
 </style>
