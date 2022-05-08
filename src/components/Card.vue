4ab0ec2441551eb7d7f4e92bd1bb4693f246baf8
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2022-05-08 15:08:14 +0530

    Created the Card page

---
+++ src/components/Card.vue
@@ -0,0 +1,21 @@
+<template>
+    <section class="courses">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="courses-card justify-content-center bg-light-brown" style="width: 18rem;">
+                    <img src="../assets/img/sample-img.png" class="card-img" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Course title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-success">View Course</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+
+<style scoped>
+    
+</style>
