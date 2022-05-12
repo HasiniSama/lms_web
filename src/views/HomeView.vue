714c7a829582109fc86bae99f9d22660e8a98236
tchamalsha <tharu.chamalsha@gmail.com>
2022-05-12 14:57:42 +0530

    add footer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -31,20 +31,23 @@
           <CourseCard />
         </div>
       </div>
+    </div>
   </div>
-  </div>
+  <Footer />
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 import NavBarVue from '@/components/NavBar.vue'
+import Footer from '@/components/Footer.vue'
 
 export default {
   name: 'HomeView',
   components: {
     HelloWorld,
-    NavBarVue
+    NavBarVue,
+    Footer
   }
 }
 </script>
