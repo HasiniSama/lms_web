5e46f83d9f3761b78b48db7c30a0c036fc2da5c5
tchamalsha <tharu.chamalsha@gmail.com>
2022-05-12 14:54:44 +0530

    add announcements

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,20 @@
 <template>
   <NavBarVue/>
-  <div class="home">
-    <HelloWorld/>
+  <div class="container-fluid p-5">
+    <div class="containr-fluid">
+      <div class="row pt-5">
+        <div class="col-md-6">
+          <h2>Hello Devid</h2>
+          <h5>Welcome back !</h5>
+        </div>
+        <div class="col-md-6">
+          <div class="announcements p-4">
+            <h3>Announcements</h3>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
