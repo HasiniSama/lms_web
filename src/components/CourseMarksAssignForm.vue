b9326d32530e4f0cdf91d67d936b673966c6d9e6
nsadisha <nsadisha@gmail.com>
2022-05-13 23:27:26 +0530

    assign marks form

---
+++ src/components/CourseMarksAssignForm.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <form method="post" @submit.prevent="assignMarks">
+            <div class="input-group input-group-sm mb-3">
+                <input type="number" min="0" max="100" class="form-control" placeholder="Marks" v-model="marks">
+                <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Assign</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+import LecturerService from '@/services/LecturerService'
+import userServices from '@/services/UserServices'
+
+export default {
+    name: 'CourseMarksAssignForm',
+    props:{
+        studentId: Number,
+        courseId: Number
+    },
+    data(){
+        return{
+            marks: "0"
+        }
+    },
+    computed:{
+        isMarksValid(){
+            return this.marks >= 0 && this.marks <= 100
+        }
+    },
+    methods:{
+        assignMarks(){
+            if(!this.isMarksValid){
+                alert("Invalid marks!")
+                return
+            }
+
+            LecturerService.assignMarks(
+                this.courseId, 
+                this.studentId, 
+                this.marks, 
+                userServices.getToken()
+            ).then(res => {
+                console.log(res)
+            }).catch(err => {
+                console.log(err)
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
