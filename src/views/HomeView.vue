02f9d7f0c425ee870168951fb32ac57f4a7aa50d
tchamalsha <tharu.chamalsha@gmail.com>
2022-05-14 17:25:32 +0530

    add dashboardforstudent option

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,41 +1,7 @@
 <template>
-  <NavBarVue/>
-  <div class="container-fluid p-5">
-    <div class="containr-fluid">
-      <div class="row pt-5">
-        <div class="col-md-6">
-          <h2>Hello {{student.name}}</h2>
-          <h5>Welcome back !</h5>
-        </div>
-        <div class="col-md-6">
-          <div class="announcements p-4">
-            <h3>Announcements</h3>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="container-fluid ">
-      <div class="row pt-5">
-        <h3 class="p-3" >Enrolled courses</h3>
-        
-          <CourseCard 
-            
-            v-for="enrolledCourse in enrolledCourses"
-            :key="enrolledCourse.course_id"
-            :code="enrolledCourse.course_code"
-            :title="enrolledCourse.name"
-            :lecturer="enrolledCourse.lecturer.name"
-            :description="enrolledCourse.description"
-
-
-          />
-
-      
-        
-      </div>
-    </div>
-  </div>
+  <NavBarVue />
+  <DashboardForStudents v-if="isStudent" />
+  <DashboardForLecturers v-else />
   <Footer />
 </template>
 
@@ -49,6 +15,8 @@ import Footer from '@/components/Footer.vue'
 import userService from '@/services/UserServices.js'
 import courseService from '@/services/CourseService.js'
 import studentService from '@/services/StudentService.js'
+import DashboardForStudents from './dashboard/DashboardForStudents.vue'
+import DashboardForLecturers from './dashboard/DashboardForLecturers.vue'
 
 export default {
   name: 'HomeView',
@@ -56,7 +24,9 @@ export default {
     HelloWorld,
     NavBarVue,
     Footer,
-    CourseCard
+    CourseCard,
+    DashboardForStudents,
+    DashboardForLecturers
   },
 
   data(){
@@ -66,52 +36,15 @@ export default {
     }
   },
 
-  methods:{
-
-    initDashboard(){
-      userService.getEnrolledCourses(
-        userService.getUserDetails().id,
-        userService.getToken()
-      ).then(res => {
-        console.log(res.data)
-        this.enrolledCourses=res.data
-      }).catch(err=>{
-        console.log(err)
-      })
-
-      userService.getStudentDetails(
-        userService.getUserDetails().id,
-        userService.getToken()
-      ).then(res => {
-        this.student = res
-      }).catch(err => {
-        console.log(err);
-      })
-      
-
+  
+  computed: {
+    isStudent(){
+      return userService.getUserDetails().role == userService.UserType.STUDENT
+     
     }
   },
 
-  created(){
-    this.initDashboard()
-  }
+  
 
 }
 </script>
-<style>
-
-h2{
-  font-size:4vh;
-  font-weight:bold;
-}
-h5{
-  font-size: 3vh;
-  color: lightgray;
-  font-weight: bold;
-}
-.announcements{
-  background-color: lightgray;
-  border-radius: 3vh;
-}
-
-</style>
