4c43cf90e4c6391cc7b3d1021be7f972b0fa7694
nsadisha <nsadisha@gmail.com>
2022-05-21 01:55:51 +0530

    is-valid class added

--- src/components/CourseMarksAssignForm.vue
+++ src/components/CourseMarksAssignForm.vue
@@ -2,7 +2,7 @@
     <div>
         <form method="post" @submit.prevent="assignMarks">
             <div class="input-group input-group-sm mb-3">
-                <input type="number" min="0" max="100" class="form-control" placeholder="Marks" v-model="marks">
+                <input type="number" min="0" max="100" class="form-control" :class="isMarksAssigned" placeholder="Marks" v-model="marks">
                 <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Assign</button>
             </div>
         </form>
@@ -21,12 +21,16 @@ export default {
     },
     data(){
         return{
-            marks: "0"
+            marks: "0",
+            isAssigned: false
         }
     },
     computed:{
         isMarksValid(){
             return this.marks >= 0 && this.marks <= 100
+        },
+        isMarksAssigned(){
+            return isSigned?'is-valid':''
         }
     },
     methods:{
@@ -42,6 +46,9 @@ export default {
                 this.marks, 
                 userServices.getToken()
             ).then(res => {
+                if(res.status == 200){
+                    this.isAssigned = true
+                }
                 console.log(res)
             }).catch(err => {
                 console.log(err)
